73c51bf4281e114d594f48e5f278ab5a56f3a3b4
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-05-07 16:46:05 +0300

    Slightly optimize generateExcludedPaths()

--- src/features/scoped-paths/generate-excluded-paths.ts
+++ src/features/scoped-paths/generate-excluded-paths.ts
@@ -1,27 +1,24 @@
 import { getParents } from '../../utils/get-parents'
-import { unique } from '../../utils/unique'
 
 export async function generateExcludedPaths(includedPaths: string[], readDirectory: (path: string) => Promise<string[]>): Promise<string[]> {
-  const includedPathsWithParents = new Set(includedPaths.flatMap((includedPath) => [
-    includedPath,
-    ...getParents(includedPath),
-  ]))
+  const includedPathsWithParents = new Set(
+    includedPaths.flatMap((includedPath) => [includedPath, ...getParents(includedPath)])
+  )
 
   const excludedPaths = new Set<string>()
 
   for (const includedPath of includedPaths) {
     const parents = getParents(includedPath)
 
-    for (const parent of parents) {
-      const children = await readDirectory(parent)
-      const filteredChildren = children.filter((child) =>
+    const children = (await Promise.all(parents.map(parent => readDirectory(parent))))
+      .flat()
+      .filter((child) =>
         !includedPathsWithParents.has(child) &&
         includedPaths.every((includedPath) => !child.startsWith(includedPath)),
       )
 
-      for (const filteredChild of filteredChildren) {
-        excludedPaths.add(filteredChild)
-      }
+    for (const child of children) {
+      excludedPaths.add(child)
     }
   }
 
