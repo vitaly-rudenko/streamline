05d11f839d9b2bb4fac69cf0776698ffd672f87d
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-05-07 17:15:14 +0300

    Fix related files feature to properly use workspace folder prefix

--- src/features/related-files/related-files-feature.ts
+++ src/features/related-files/related-files-feature.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import { isMultiRootWorkspace } from '../../utils/is-multi-root-workspace'
 
 export async function createRelatedFilesFeature(input: {
   context: vscode.ExtensionContext
@@ -6,14 +7,14 @@ export async function createRelatedFilesFeature(input: {
   const { context } = input
 
   context.subscriptions.push(
-    vscode.commands.registerCommand('streamline.quick-open-related-files', async (file: vscode.Uri | undefined) => {
-      file ||= vscode.window.activeTextEditor?.document.uri
-      if (!file) return
+    vscode.commands.registerCommand('streamline.quick-open-related-files', async (uri: vscode.Uri | undefined) => {
+      uri ||= vscode.window.activeTextEditor?.document.uri
+      if (!uri) return
 
-      const workspaceFolder = vscode.workspace.getWorkspaceFolder(file)
-      const pathWithoutExtension = vscode.workspace.asRelativePath(file).replace(/\..*$/, '')
-      const filename = pathWithoutExtension.split('/').at(-1)
+      const filename = uri.path.replace(/\..+$/, '').replace(/^.+\//, '')
       if (!filename) return
+
+      const workspaceFolder = isMultiRootWorkspace() ? vscode.workspace.workspaceFolders?.[0] : undefined
 
       await vscode.commands.executeCommand('workbench.action.quickOpen', workspaceFolder ? `${workspaceFolder.name}/${filename}` : filename)
     })
