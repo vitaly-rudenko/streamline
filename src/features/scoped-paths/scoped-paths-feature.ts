c9f56d815f0a7778def13bf23c4b29c35311c127
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-05-08 15:17:06 +0300

    Added support for multi-scopes, added status bar item and more commands, and improvements

--- src/features/scoped-paths/scoped-paths-feature.ts
+++ src/features/scoped-paths/scoped-paths-feature.ts
@@ -4,13 +4,17 @@ import { readDirectory } from '../../utils/read-directory'
 import { serializeExcludes } from './serialize-excludes'
 import { getParents } from '../../utils/get-parents'
 import { uriToPath } from '../../utils/uri'
+import { unique } from '../../utils/unique'
 
 export async function createScopedPathsFeature(input: {
   context: vscode.ExtensionContext
   onScopeChanged: (payload: vscode.Uri | vscode.Uri[] | undefined) => unknown
 }) {
   const { context, onScopeChanged } = input
-	const workspaceFilesConfig = vscode.workspace.getConfiguration('files', null)
+
+  const textStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100)
+  textStatusBarItem.command = 'streamline.select-scope'
+  context.subscriptions.push(textStatusBarItem)
 
   let cachedScopedPaths: Set<string> = new Set()
   let cachedParentScopedPaths: Set<string> = new Set()
@@ -24,33 +28,47 @@ export async function createScopedPathsFeature(input: {
   }
 
   async function toggleScopeForFile(path: string) {
-    const scopedPaths = await vscode.workspace.getConfiguration('streamline').get<string[]>('scopedPaths', [])
-    await vscode.workspace.getConfiguration('streamline').update(
-      'scopedPaths',
-      scopedPaths.includes(path)
-        ? scopedPaths.filter(scopedPaths => scopedPaths !== path)
-        : [...scopedPaths, path],
-    )
+    const config = vscode.workspace.getConfiguration('streamline')
+    const scopes = config.get<Record<string, string[]>>('scopes', {})
+    const currentScope = config.get<string>('currentScope', 'default')
+
+    const scopedPaths = scopes[currentScope] ?? []
+    scopes[currentScope] = scopedPaths.includes(path)
+      ? scopedPaths.filter(scopedPaths => scopedPaths !== path)
+      : [...scopedPaths, path]
+
+    await config.update('scopes', scopes)
   }
 
   async function refresh() {
     const config = vscode.workspace.getConfiguration('streamline')
 
-    const scopedPaths = config.get<string[]>('scopedPaths', [])
+    const scopes = config.get<Record<string, string[]>>('scopes', {})
+    const currentScope = config.get<string>('currentScope', 'default')
     const scopeEnabled = config.get<boolean>('scopeEnabled', false)
 
+    const scopedPaths = scopes[currentScope] ?? []
     cachedScopedPaths = new Set(scopedPaths)
     cachedParentScopedPaths = new Set(scopedPaths.flatMap(scopedPath => getParents(scopedPath)))
 
-    if (scopeEnabled) {
-      const excludedPaths = await generateExcludedPaths(scopedPaths, readDirectory)
-      const excludes = serializeExcludes({ excludedPaths })
-      await workspaceFilesConfig.update('exclude', excludes, vscode.ConfigurationTarget.Workspace)
-    } else {
-      await workspaceFilesConfig.update('exclude', undefined, vscode.ConfigurationTarget.Workspace)
+    try {
+      const workspaceFilesConfig = vscode.workspace.getConfiguration('files', null)
+      if (scopeEnabled) {
+        const excludedPaths = await generateExcludedPaths(scopedPaths, readDirectory)
+        const excludes = serializeExcludes({ excludedPaths })
+        await workspaceFilesConfig.update('exclude', excludes, vscode.ConfigurationTarget.Workspace)
+      } else {
+        await workspaceFilesConfig.update('exclude', undefined, vscode.ConfigurationTarget.Workspace)
+      }
+    } catch (err) {
+      console.warn('Could not update workspace configuration', err)
     }
 
     await vscode.commands.executeCommand('setContext', 'streamline.scoped', scopeEnabled)
+
+    textStatusBarItem.text = `Scope: ${currentScope}`
+    textStatusBarItem.backgroundColor = scopeEnabled ? new vscode.ThemeColor('statusBarItem.warningBackground') : undefined
+    textStatusBarItem.show()
 
     onScopeChanged(undefined)
   }
@@ -65,6 +83,13 @@ export async function createScopedPathsFeature(input: {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('streamline.unscope', async () => {
       await vscode.workspace.getConfiguration('streamline').update('scopeEnabled', false)
+		})
+	)
+
+  context.subscriptions.push(
+		vscode.commands.registerCommand('streamline.toggle-scope', async () => {
+      const config = vscode.workspace.getConfiguration('streamline')
+      await config.update('scopeEnabled', !config.get('scopeEnabled', false))
 		})
 	)
 
@@ -107,6 +132,39 @@ export async function createScopedPathsFeature(input: {
 		})
 	)
 
+  context.subscriptions.push(
+    vscode.commands.registerCommand('streamline.select-scope', async () => {
+      const config = vscode.workspace.getConfiguration('streamline')
+      const scopes = config.get<Record<string, string[]>>('scopes', {})
+
+      let scope = await vscode.window.showQuickPick(
+        unique(['default', ...Object.keys(scopes), '+ Add new scope']),
+        { title: 'Select a scope' }
+      )
+
+      if (!scope) return
+
+      if (scope === '+ Add new scope') {
+        scope = await vscode.window.showInputBox({ prompt: 'Enter the name of new scope' })
+        if (!scope) return
+        await config.update('scopes', { ...scopes, [scope]: [] })
+      }
+
+      await config.update('currentScope', scope)
+      await refresh()
+    })
+  )
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('streamline.clear-scope', async () => {
+      const config = vscode.workspace.getConfiguration('streamline')
+      const scopes = config.get<Record<string, string[]>>('scopes', {})
+      const currentScope = config.get<string>('currentScope', 'default')
+
+      await config.update('scopes', { ...scopes, [currentScope]: [] })
+      await refresh()
+    })
+  )
 
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration(async (event) => {
