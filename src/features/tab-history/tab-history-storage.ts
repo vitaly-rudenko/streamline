cf4485a791fd1b246f2afd972a09f7406de5c28a
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-05-18 14:14:57 +0300

    Added basic TabHistoryStorage implementation with tests

---
+++ src/features/tab-history/tab-history-storage.ts
@@ -0,0 +1,58 @@
+import type { Tab } from './types'
+
+export class TabHistoryStorage {
+  private tabs: Tab[] = []
+
+  constructor(private readonly memorySize: number) {}
+
+  import(backup: Record<string, number>) {
+    const tabsToImport = Object.entries(backup)
+      .map(([path, openedAt]) => ({ path, openedAt }))
+      .sort((a, b) => b.openedAt - a.openedAt)
+
+    const existingPaths = new Set<string>(this.tabs.map(tab => tab.path))
+    for (const tab of tabsToImport) {
+      if (existingPaths.has(tab.path)) continue
+      this.tabs.push(tab)
+    }
+
+    this.tabs = this.tabs.slice(0, this.memorySize)
+  }
+
+  export(backupSize: number): Record<string, number> {
+    return this.tabs
+      .sort((a, b) => b.openedAt - a.openedAt)
+      .slice(0, backupSize)
+      .reduce<Record<string, number>>((acc, tab) => {
+        acc[tab.path] = tab.openedAt
+        return acc
+      }, {})
+  }
+
+  put(tab: Tab) {
+    const index = this.tabs.findIndex(t => t.path === tab.path)
+
+    if (index !== -1) {
+      this.tabs[index] = tab
+      return
+    }
+
+    this.tabs.unshift(tab)
+    if (this.tabs.length > this.memorySize) {
+      const oldestTabIndex = this.tabs.reduce((oldestIndex, tab, tabIndex) => tab.openedAt < this.tabs[oldestIndex].openedAt ? tabIndex : oldestIndex, 0)
+      this.tabs.splice(oldestTabIndex, 1)
+    }
+  }
+
+  sort() {
+    this.tabs.sort((a, b) => b.openedAt - a.openedAt)
+  }
+
+  clear() {
+    this.tabs = []
+  }
+
+  list() {
+    return [...this.tabs]
+  }
+}
