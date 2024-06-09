7f21373fe32dfaedd9c28466361f38466d63ebf4
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-06-09 18:31:03 +0300

    Current list should always be shown in the view

--- src/features/bookmarks/bookmarks-tree-data-provider.ts
+++ src/features/bookmarks/bookmarks-tree-data-provider.ts
@@ -23,7 +23,7 @@ export class BookmarksTreeDataProvider implements vscode.TreeDataProvider<TreeIt
 
   async getChildren(element?: TreeItem): Promise<TreeItem[] | undefined> {
     if (element === undefined) {
-      return unique(this.config.getBookmarks().map(bookmark => bookmark.list))
+      return unique([...this.config.getBookmarks().map(bookmark => bookmark.list), this.config.getCurrentList()])
         .sort()
         .map(list => new ListTreeItem(list, this.config.getCurrentList() === list))
     }
