e790cf44fc2dea747d24be9a8437b68bda386d0b
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-06-19 00:14:35 +0300

    More forgiving related files query to include files that end with basename, not just ones that start with it

--- src/features/related-files/get-related-files-queries.ts
+++ src/features/related-files/get-related-files-queries.ts
@@ -4,8 +4,15 @@ export function getRelatedFilesQueries(path: string) {
   const basename = getBasename(path)
   const singleFolder = path.split('/').at(-2)
 
+  if (singleFolder) {
+    return {
+      best: `**/${singleFolder}/${basename}*`,
+      worst: `**/*${basename}*`
+    }
+  }
+
   return {
-    best: `**/${singleFolder ? `${singleFolder}/${basename}` : basename}*`,
-    worst: `**/${basename}*`
+    best: `**/${basename}*`,
+    worst: `**/*${basename}*`
   }
 }
