6e486f26497b926505a30394ac46c3653c6fb189
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-06-23 15:15:24 +0300

    Handle non-existing and non-folder reads in CachedDirectoryReader

--- src/utils/cached-directory-reader.ts
+++ src/utils/cached-directory-reader.ts
@@ -1,24 +1,29 @@
 import * as vscode from 'vscode'
 import { pathToUri } from './uri'
+import type { DirectoryReader } from './types'
 
-export class CachedDirectoryReader {
+export class CachedDirectoryReader implements DirectoryReader {
 	private _cache = new Map<string, string[]>()
 
 	async read(path: string): Promise<string[]> {
 		const cached = this._cache.get(path)
 		if (cached) return cached
 
-		let results: string[]
+		let results: string[] = []
 		if (path !== '') {
 			const uri = pathToUri(path)
 			if (uri) {
-				const files = await vscode.workspace.fs.readDirectory(uri)
-				results = files.map(([name]) => `${path}/${name}`)
-			} else {
-				results = []
+				try {
+					const files = await vscode.workspace.fs.readDirectory(uri)
+					results = files.map(([name]) => `${path}/${name}`)
+				} catch (error) {
+					if (!(error instanceof vscode.FileSystemError && (error.code === 'FileNotADirectory' || error.code === 'FileNotFound'))) {
+						throw error
+					}
+				}
 			}
 		} else {
-			results = vscode.workspace.workspaceFolders?.map(workspaceFolder => workspaceFolder.name) ?? []
+			results = (vscode.workspace.workspaceFolders ?? []).map(workspaceFolder => workspaceFolder.name)
 		}
 
 		this._cache.set(path, results)
