7c31279ac3d2cd8024e14998dc4dd85e7c66aa92
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-07-27 10:54:32 +0300

    Retry saving settings in background with interval

--- src/features/feature-config.ts
+++ src/features/feature-config.ts
@@ -1,3 +1,6 @@
+const MAX_ATTEMPTS = 5
+const MIN_ATTEMPT_INTERVAL_MS = 1000
+
 export abstract class FeatureConfig {
   private _saveInBackgroundQueue = Promise.resolve()
   private _isSavingInBackground = false
@@ -7,13 +10,20 @@ export abstract class FeatureConfig {
   abstract load(): boolean
   abstract save(): Promise<void>
 
-  async saveInBackground() {
+  async saveInBackground(attempt = 1) {
     this._saveInBackgroundQueue = this._saveInBackgroundQueue
       .then(() => {
         this._isSavingInBackground = true
+        console.log(`[${this.featureName}] Saving config in background (attempt: ${attempt})`)
         return this.save()
       })
-      .catch((error) => console.warn(`[${this.featureName}] Could not save config`, error))
+      .catch((error) => {
+        console.warn(`[${this.featureName}] Could not save config in background (attempt: ${attempt})`, error)
+
+        if (attempt < MAX_ATTEMPTS) {
+          setTimeout(() => this.saveInBackground(attempt + 1), attempt * MIN_ATTEMPT_INTERVAL_MS)
+        }
+      })
       .finally(() => this._isSavingInBackground = false)
 
     return this._saveInBackgroundQueue
