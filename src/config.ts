2ff18bb34a8de07bd21ecb23835a38bcdc473402
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-08-16 14:42:04 +0300

    Added comments

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,9 @@ export function getConfig() {
   return vscode.workspace.getConfiguration('streamline')
 }
 
-// Get the ConfigurationTarget (read: scope) of where the *effective* setting value comes from
+/**
+ * Get real configuration target of the config section – a.k.a. where it's currently set.
+ */
 export function getEffectiveConfigurationTarget(config: vscode.WorkspaceConfiguration, section: string): vscode.ConfigurationTarget | undefined {
   const detail = config.inspect(section)
   if (detail === undefined) {
@@ -20,10 +22,20 @@ export function getEffectiveConfigurationTarget(config: vscode.WorkspaceConfigur
   return undefined
 }
 
+/**
+ * Used instead of `config.has()` because `config.has()` returns true even when value is not set,
+ * most probably because of the `defaultValue`.
+ *
+ * Getting effective configuration target is a more reliable way to check if config section is set.
+ */
 export function configExists(config: vscode.WorkspaceConfiguration, section: string): boolean {
   return getEffectiveConfigurationTarget(config, section) !== undefined
 }
 
+/**
+ * Update config section in the place where it's currently set,
+ * otherwise use default target.
+ */
 export async function updateEffectiveConfig(config: vscode.WorkspaceConfiguration, section: string, value: any, defaultTarget: vscode.ConfigurationTarget) {
   const target = getEffectiveConfigurationTarget(config, section) ?? defaultTarget
   await config.update(section, value, target)
