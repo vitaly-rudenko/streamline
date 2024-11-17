546ce328c1cf533a2351c2c2613dbb61fbfa1c3f
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-11-17 21:12:04 +0200

    Updates

--- src/features/super-search/patterns.ts
+++ src/features/super-search/patterns.ts
@@ -1,31 +1,45 @@
+export type PatternType = keyof typeof patterns
+
+export type PatternOptions = {
+  wholeWord: boolean
+}
+
 export const patterns = {
   findInAllNamingConventions: (words: string[]): string => {
     return words
-      .map(word => escapeRegExp(word))
+      .map(word => escape(word))
       .join('[-_]?')
   },
-  findLinesWithAllWordsInProvidedOrder: (words: string[]): string => {
+  findLinesWithAllWordsInProvidedOrder: (words: string[], options?: PatternOptions): string => {
     return words
-      .map(word => escapeRegExp(word))
+      .map(word => escape(word))
+      .map(word => options?.wholeWord ? bounded(word) : word)
       .join('.*?')
   },
-  findLinesWithAllWordsInAnyOrder: (words: string[]): string => {
+  findLinesWithAllWordsInAnyOrder: (words: string[], options?: PatternOptions): string => {
     return '^' + words
-      .map(word => escapeRegExp(word))
+      .map(word => escape(word))
+      .map(word => options?.wholeWord ? bounded(word) : word)
       .map(escapedWord => `(?=.*?${escapedWord})`).join('') + '.*$'
   },
-  findFilesWithAllWordsInProvidedOrder: (words: string[]): string => {
+  findFilesWithAllWordsInProvidedOrder: (words: string[], options?: PatternOptions): string => {
     return words
-      .map(word => escapeRegExp(word))
+      .map(word => escape(word))
+      .map(word => options?.wholeWord ? bounded(word) : word)
       .join('(?:.|\\n)*?')
   },
-  findFilesWithAllWordsInAnyOrder: (words: string[]): string => {
+  findFilesWithAllWordsInAnyOrder: (words: string[], options?: PatternOptions): string => {
     return '^' + words
-      .map(word => escapeRegExp(word))
+      .map(word => escape(word))
+      .map(word => options?.wholeWord ? bounded(word) : word)
       .map(escapedWord => `(?=(?:.|\\n)*?${escapedWord})`).join('') + '(?:.|\\n)*$'
   },
 }
 
-function escapeRegExp(input: string) {
+function escape(input: string) {
   return input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
+function bounded(input: string): string {
+  return `\\b${input}\\b`
 }
