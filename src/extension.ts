aec040256cd6d31b5ad67091568f0c7a608e8259
Vitaly Rudenko <maurice.dederick@gmail.com>
2025-05-20 15:02:41 +0300

    hasFoldedRegions -> hasVisibleFoldedRegions

--- src/extension.ts
+++ src/extension.ts
@@ -61,7 +61,7 @@ export function activate(context: vscode.ExtensionContext) {
         untitled: input.document.isUntitled,
         fileType: 'file',
         selection: !input.selection.isEmpty,
-        hasFoldedRegions: input.visibleRanges.length > 1,
+        hasVisibleFoldedRegions: input.visibleRanges.length > 1,
         hasBreakpoints: vscode.debug.breakpoints.some(
           breakpoint => breakpoint instanceof vscode.SourceBreakpoint
                      && breakpoint.location.uri.path === input.document.uri.path
