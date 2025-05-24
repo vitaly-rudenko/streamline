655c225a68b9845fea748a2918d36ab56aa427fa
Vitaly Rudenko <maurice.dederick@gmail.com>
2025-05-25 01:37:14 +0300

    Consistent naming

--- src/features/highlighted-paths/highlighted-paths-feature.ts
+++ src/features/highlighted-paths/highlighted-paths-feature.ts
@@ -10,18 +10,19 @@ export function createHighlightedPathsFeature(input: {
 
   const config = new HighlightedPathsConfig()
 
-  const scheduleConfigLoad = createDebouncedFunction(() => {
+  const debouncedHandleConfigChanged = createDebouncedFunction(() => {
     if (!config.load()) return
+
     onChange()
   }, 500)
 
-  context.subscriptions.push(scheduleConfigLoad)
+  context.subscriptions.push(debouncedHandleConfigChanged)
 
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration((event) => {
       if (event.affectsConfiguration('streamline.highlightedPaths')) {
         if (!config.isSavingInBackground) {
-          scheduleConfigLoad.schedule()
+          debouncedHandleConfigChanged.schedule()
         }
       }
     }),
